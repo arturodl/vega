be562b327b747b02087daabcb166dbee65bd1ff5
arturodl <arturo.04@gmail.com>
2017-04-25 16:53:41 -0500

    trying a responsive design on login

---
+++ Glassfish/PrimefacesMCApp/src/com/primefacesmc/beans/aplicacion/LoginBean.java
@@ -0,0 +1,47 @@
+package com.primefacesmc.beans.aplicacion;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean(name="loginBean")
+@ViewScoped
+public class LoginBean {
+	private String redireccionar;
+	private String nombreUsuario;
+	private String claveUsuario;	
+	
+	public void onLogin(ActionEvent event){
+		System.out.println("Bienvenido al sistema:"+this.nombreUsuario+", tu clave es: "+this.claveUsuario);
+		this.redireccionar = "principal";
+	}
+	
+	public String redireccionar(){
+		return this.redireccionar;
+	}
+
+	public String getRedireccionar() {
+		return redireccionar;
+	}
+
+	public void setRedireccionar(String redireccionar) {
+		this.redireccionar = redireccionar;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getClaveUsuario() {
+		return claveUsuario;
+	}
+
+	public void setClaveUsuario(String claveUsuario) {
+		this.claveUsuario = claveUsuario;
+	}
+
+}
