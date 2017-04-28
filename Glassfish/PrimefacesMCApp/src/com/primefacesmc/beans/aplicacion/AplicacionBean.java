a0403d18fbd65c403ccadca50d80bafed46c6174
arturodl <arturo.04@gmail.com>
2017-04-28 10:49:36 -0500

    commiting new user options

--- Glassfish/PrimefacesMCApp/src/com/primefacesmc/beans/aplicacion/AplicacionBean.java
+++ Glassfish/PrimefacesMCApp/src/com/primefacesmc/beans/aplicacion/AplicacionBean.java
@@ -1,7 +1,12 @@
 package com.primefacesmc.beans.aplicacion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.ViewScoped;
 
 import org.primefaces.model.DefaultTreeNode;
@@ -14,6 +19,7 @@ public class AplicacionBean {
     private String usuario;
     private String password;
     private TreeNode raizArbol;
+    private Map<String, String> pagina;
     
     @PostConstruct
     public void inicializar(){
@@ -53,10 +59,29 @@ public class AplicacionBean {
     	reportes.getChildren().add(new DefaultTreeNode("Reporte Tipo E"));
     	
     	this.usuario = "Arturo Linares";
+    	
+    	pagina = new HashMap<String, String>();
+    	this.pagina.put("url", "/WEB-INF/web/content/pantallas/usuario/Usuario-Inicio.xhtml");
     }
     
-    public void validando(){
-    	System.out.println("Validando el enlace");
+    
+    public void actualizarContenido(AjaxBehaviorEvent eventoAjax){
+    	System.out.println("Actualizando contenido, agregando nuevo enlace, boton clicleado: "+eventoAjax.getComponent().getId());
+    	
+    	if ( eventoAjax.getComponent().getId().compareTo("headerPlantilla-linkInicio") == 0)
+    	  this.pagina.put("url", "/WEB-INF/web/content/pantallas/usuario/Usuario-Inicio.xhtml");
+    	else 
+    		if ( eventoAjax.getComponent().getId().compareTo("headerPlantilla-linkCorreos") == 0)
+    			this.pagina.put("url", "/WEB-INF/web/content/pantallas/usuario/Usuario-Correos.xhtml");
+    	else 
+    		if ( eventoAjax.getComponent().getId().compareTo("headerPlantilla-linkNotificaciones") == 0)
+    			this.pagina.put("url", "/WEB-INF/web/content/pantallas/usuario/Usuario-Notificaciones.xhtml");
+    	else 
+    		if ( eventoAjax.getComponent().getId().compareTo("headerPlantilla-linkUsuario") == 0)
+    			this.pagina.put("url", "/WEB-INF/web/content/pantallas/usuario/Usuario-DatosUsuario.xhtml");
+    	else 
+    		if ( eventoAjax.getComponent().getId().compareTo("headerPlantilla-linkConfiguracion") == 0)
+    			this.pagina.put("url", "/WEB-INF/web/content/pantallas/usuario/Usuario-Configuracion.xhtml");
     }
 
 	public int getClave() {
@@ -89,5 +114,13 @@ public class AplicacionBean {
 
 	public void setRaizArbol(TreeNode raizArbol) {
 		this.raizArbol = raizArbol;
+	}
+
+	public Map<String, String> getPagina() {
+		return pagina;
+	}
+
+	public void setPagina(Map<String, String> pagina) {
+		this.pagina = pagina;
 	}
 }
